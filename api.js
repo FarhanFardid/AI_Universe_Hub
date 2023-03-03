af122f8e193ac8265adb4fe15a62f2d31f0a6056
FarhanFardid <farhanfardid009@gmail.com>
2023-03-03 20:11:16 +0600

    data loaded successfully and cards display

--- api.js
+++ api.js
@@ -1,1 +1,36 @@
+// data load via api
+
+const loadData = async () =>{
+    const url = `https://openapi.programming-hero.com/api/ai/tools`
+    const res = await fetch(url);
+    const data = await res.json();
+    displayData(data.data.tools);
+}
+// display tha loaded data
+const displayData= (results) =>{
+    const infoContainer = document.getElementById('info_container');
+    for (const result of results){
+        const cardDiv = document.createElement('div');
+        cardDiv.classList.add('col');
+        cardDiv.innerHTML =`
+        <div class="card h-100">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        </div>
+        <div class="card-footer">
+          <small class="text-muted">Last updated 3 mins ago</small>
+        </div>
+      </div>
+        `
+        infoContainer.appendChild(cardDiv);
+        
+        console.log(result);
+    }
+    
+
+} 
+
+loadData();
 console.log("Connected");
