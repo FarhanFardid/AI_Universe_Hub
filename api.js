090541a39dd40f937600528d6c5fbcd684373264
FarhanFardid <farhanfardid009@gmail.com>
2023-03-03 20:20:44 +0600

    crads details added

--- api.js
+++ api.js
@@ -14,18 +14,20 @@ const displayData= (results) =>{
         cardDiv.classList.add('col');
         cardDiv.innerHTML =`
         <div class="card h-100">
-        <img src="..." class="card-img-top" alt="...">
+        <img src="${result.image}" class="card-img-top" alt="...">
         <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <h5 class="card-title">Features</h5>
+          <p class="card-text"></p>
+          
         </div>
         <div class="card-footer">
+        <h3 class="card-title">${result.name}</h3>
           <small class="text-muted">Last updated 3 mins ago</small>
         </div>
       </div>
         `
         infoContainer.appendChild(cardDiv);
-        
+
         console.log(result);
     }
     
