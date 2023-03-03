658f3ad8c3dca05a3a48791b62a8ba5efa9a4e30
FarhanFardid <farhanfardid009@gmail.com>
2023-03-03 21:24:15 +0600

    card button added

--- api.js
+++ api.js
@@ -14,15 +14,28 @@ const displayData= (results) =>{
         cardDiv.classList.add('col');
         cardDiv.innerHTML =`
         <div class="card h-100">
-        <img src="${result.image}" class="card-img-top" alt="...">
+        <img src="${result.image}" class="card-img-top p-4 border rounded-3" alt="...">
         <div class="card-body">
         <h5 class="card-title">Features</h5>
-          <p class="card-text"></p>
+          <p class="card-text">
+          <ol>
           
+          <li>${result.features[0]}</li>
+          <li>${result.features[1]}</li>
+          <li>${result.features[2] ? result.features[2]: 'ChatBox'} </li>
+        </ol>
+          </p>
+        
         </div>
         <div class="card-footer">
         <h3 class="card-title">${result.name}</h3>
-          <small class="text-muted">Last updated 3 mins ago</small>
+        <div class="row row-cols-2">
+       <div class="col-10"><small class="text-muted " ><i class="fa-regular fa-calendar-days p-1"></i>${result.published_in}</small></div>
+
+        <div class="col-2"><button class="btn btn-primary"><i class="fa-solid fa-arrow-right"></i></button></div>
+     
+        </div>
+
         </div>
       </div>
         `
